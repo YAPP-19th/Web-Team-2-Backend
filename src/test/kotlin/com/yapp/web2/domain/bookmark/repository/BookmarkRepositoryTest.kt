8c1b282b3a183e1807ff31e2dc7ceb2fcd815da1
Ji-Ha <xodhkd36@naver.com>
2021-10-27 23:38:49 +0900

    test: BookmarkRepository 연결확인 test

---
+++ src/test/kotlin/com/yapp/web2/domain/bookmark/repository/BookmarkRepositoryTest.kt
@@ -0,0 +1,27 @@
+package com.yapp.web2.domain.bookmark.repository
+
+import com.yapp.web2.domain.bookmark.entity.Bookmark
+import com.yapp.web2.domain.bookmark.entity.Url
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+
+@DataMongoTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+internal class BookmarkRepositoryTest {
+    @Autowired
+    lateinit var bookmarkRepository: BookmarkRepository
+
+    @Test
+    fun `bookmark가 mongoDB에 저장된다`() {
+        // given
+        val url = Url("www.naver.com", "", 0)
+        val bookmark = Bookmark(1, 1, url)
+
+        //when, then
+        Assertions.assertThat(bookmarkRepository.save(bookmark)).isEqualTo(bookmark)
+    }
+}
