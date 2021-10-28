342b3aab684d31027e4fc5d570012e65ffa20a7e
Ji-Ha <xodhkd36@naver.com>
2021-10-28 20:23:17 +0900

    test: url을 추가할 때, 폴더가 존재하지 않으면 예외를 던진다.

--- src/test/kotlin/com/yapp/web2/domain/bookmark/service/BookmarkServiceTest.kt
+++ src/test/kotlin/com/yapp/web2/domain/bookmark/service/BookmarkServiceTest.kt
@@ -4,47 +4,75 @@ import com.yapp.web2.domain.bookmark.entity.Bookmark
 import com.yapp.web2.domain.bookmark.entity.Url
 import com.yapp.web2.domain.bookmark.entity.UrlDto
 import com.yapp.web2.domain.bookmark.repository.BookmarkRepository
+import com.yapp.web2.domain.folder.repository.FolderRepository
+import com.yapp.web2.exception.BusinessException
+import com.yapp.web2.exception.GlobalExceptionHandler
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.*
+import java.util.*
 
 internal class BookmarkServiceTest {
     @MockK
     private lateinit var bookmarkRepository: BookmarkRepository
+
+    @MockK
+    private lateinit var folderRepository: FolderRepository
     private lateinit var bookmarkService: BookmarkService
+    lateinit var globalExceptionHandler: GlobalExceptionHandler
 
     @BeforeEach
     internal fun init() {
         MockKAnnotations.init(this)
-        bookmarkService = BookmarkService(bookmarkRepository)
+        globalExceptionHandler = GlobalExceptionHandler()
+        bookmarkService = BookmarkService(bookmarkRepository, folderRepository)
     }
 
     @Nested
     inner class SaveUrl {
         private lateinit var bookmark: Bookmark
         private lateinit var url: Url
+        private lateinit var urlDto: UrlDto
 
         @BeforeEach
         internal fun setUp() {
             url = Url("www.naver.com", "", 0)
             bookmark = Bookmark(1, 1, url)
+            urlDto = UrlDto("www.naver.com")
         }
 
         @Test
         fun `폴더가 존재하고, URL을 추가한다`() {
             // given
-            val urlDto = UrlDto("www.naver.com")
             every { bookmarkRepository.save(any()) } returns bookmark
 
             // when
             val addBookmark = bookmarkService.addBookmark(1, urlDto)
 
             // then
-            Assertions.assertThat(addBookmark).isEqualTo(bookmark)
+            assertThat(addBookmark).isEqualTo(bookmark)
+        }
+
+        @Test
+        fun `url을 추가할 때, 폴더가 존재하지 않으면 예외를 던진다`() {
+            //given
+            every { folderRepository.findById(1) } returns Optional.empty()
+            val businessException = BusinessException("없어요.")
+
+            //when
+            val exception = Assertions.assertThrows(BusinessException::class.java) {
+                bookmarkService.addBookmark(1, urlDto)
+            }
+
+            //then
+            Assertions.assertEquals(businessException.message, exception.message)
+        }
+
+        @Test
+        fun `같은 url이 존재한다면, 예외가 발생한다`() {
+
         }
     }
 
