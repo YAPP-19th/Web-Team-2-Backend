90b3feb357476d895521defb038f5107f9ab134d
Ji-Ha <xodhkd36@naver.com>
2021-11-02 16:14:28 +0900

    feature(pagination): 북마크를 기준에 맞게 가져오는 기능 추가

--- src/main/kotlin/com/yapp/web2/domain/bookmark/repository/BookmarkRepository.kt
+++ src/main/kotlin/com/yapp/web2/domain/bookmark/repository/BookmarkRepository.kt
@@ -1,10 +1,14 @@
 package com.yapp.web2.domain.bookmark.repository
 
 import com.yapp.web2.domain.bookmark.entity.Bookmark
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface BookmarkRepository : MongoRepository<Bookmark, Long> {
-    fun findAllByFolderId(folderId: Long): List<Bookmark>
+    fun findAllByFolderIdAndRemindTimeIsNotNull(FolderId: Long, pageable: Pageable): Page<Bookmark>
+    fun findAllByFolderId(FolderId: Long, pageable: Pageable): Page<Bookmark>
+    fun findAllByFolderId(FolderId: Long): List<Bookmark>
 }
