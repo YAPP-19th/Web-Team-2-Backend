4fbae7cd7562f36c346bdb2584dea64b354c0269
JuHyun419 <zzang9ha@naver.com>
2021-11-12 19:44:49 +0900

    [#14] feat(Security 의존성 추가)

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-web")
