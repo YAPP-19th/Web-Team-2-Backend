41ffedf81acbfd705dc2b4cb21ee68e1c4c95fbd
Ji-Ha <xodhkd36@naver.com>
2021-12-03 16:25:22 +0900

    [#23] feature(닉네임변경) : 닉네임변경 기능 추가

--- src/main/kotlin/com/yapp/web2/domain/account/service/AccountService.kt
+++ src/main/kotlin/com/yapp/web2/domain/account/service/AccountService.kt
@@ -1,21 +1,22 @@
 package com.yapp.web2.domain.account.service
 
 import com.yapp.web2.domain.account.entity.Account
-import com.yapp.web2.domain.account.repository.UserRepository
+import com.yapp.web2.domain.account.repository.AccountRepository
+import com.yapp.web2.exception.BusinessException
 import com.yapp.web2.security.jwt.JwtProvider
 import com.yapp.web2.security.jwt.TokenDto
 import org.springframework.stereotype.Service
 import javax.transaction.Transactional
 
 @Service
-class UserService(
-    private val userRepository: UserRepository,
+class AccountService(
+    private val accountRepository: AccountRepository,
     private val jwtProvider: JwtProvider
 ) {
     fun oauth2LoginUser(dto: Account.AccountLoginRequest): TokenDto {
         var account = Account.requestToAccount(dto)
 
-        account = when (val savedAccount = userRepository.findByEmail(account.email)) {
+        account = when (val savedAccount = accountRepository.findByEmail(account.email)) {
             null -> createUser(account)
             else -> updateUser(savedAccount, account)
         }
@@ -32,10 +33,20 @@ class UserService(
 
     @Transactional
     fun createUser(account: Account): Account {
-        return userRepository.save(account)
+        return accountRepository.save(account)
     }
 
     fun reIssuedAccessToken(accessToken: String, refreshToken: String): TokenDto {
         return jwtProvider.reIssuedAccessToken(accessToken, refreshToken)
+    }
+
+    @Transactional
+    fun changeNickName(token: String, nextNickName: Account.nextNickName): Unit {
+        val idFromToken = jwtProvider.getIdFromToken(token)
+        val account = accountRepository.findById(idFromToken).let {
+            if (it.isEmpty) throw BusinessException("계정이 존재하지 않습니다.")
+            it.get().nickname = nextNickName.nickName
+            it
+        }
     }
 }
