5fb5351d46f5a572b80c920a01a264335985feb9
JuHyun419 <zzang9ha@naver.com>
2021-12-04 21:23:34 +0900

    [#17] feat(Swagger) : 북마크 Swagger 설정

--- src/main/kotlin/com/yapp/web2/domain/bookmark/controller/BookmarkController.kt
+++ src/main/kotlin/com/yapp/web2/domain/bookmark/controller/BookmarkController.kt
@@ -4,6 +4,8 @@ import com.yapp.web2.domain.bookmark.entity.Bookmark
 import com.yapp.web2.domain.bookmark.service.BookmarkPageService
 import com.yapp.web2.domain.bookmark.service.BookmarkSearchService
 import com.yapp.web2.domain.bookmark.service.BookmarkService
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
@@ -19,6 +21,7 @@ class BookmarkController(
     private val bookmarkService: BookmarkService,
     private val bookmarkSearchService: BookmarkSearchService
 ) {
+    // TODO: 2021/12/04 Swagger 추가
     @GetMapping("/{folderId}")
     fun getBookmarkPage(
         @PathVariable folderId: Long,
@@ -33,41 +36,46 @@ class BookmarkController(
         return ResponseEntity.status(HttpStatus.OK).body("올라감")
     }
 
+    @ApiOperation(value = "북마크 생성 API")
     @PostMapping("/{folderId}")
     fun createBookmark(
         request: HttpServletRequest,
-        @PathVariable folderId: Long,
-        @RequestBody bookmark: Bookmark.AddBookmarkDto
+        @PathVariable @ApiParam(value = "북마크를 지정할 폴더 ID", example = "12", required = true) folderId: Long,
+        @RequestBody @ApiParam(value = "북마크 생성 정보", required = true) bookmark: Bookmark.AddBookmarkDto
     ): ResponseEntity<String> {
         val token = request.getHeader("AccessToken")
         bookmarkService.addBookmark(token, folderId, bookmark)
         return ResponseEntity.status(HttpStatus.OK).body("저장됨")
     }
 
+    @ApiOperation(value = "북마크 삭제 API")
     @DeleteMapping("/{bookmarkId}")
-    fun deleteBookmark(@PathVariable bookmarkId: String): ResponseEntity<String> {
+    fun deleteBookmark(@PathVariable @ApiParam(value = "북마크 ID", example = "10", required = true) bookmarkId: String): ResponseEntity<String> {
         bookmarkService.deleteBookmark(bookmarkId)
         return ResponseEntity.status(HttpStatus.OK).body("삭제됨")
     }
 
+    @ApiOperation(value = "북마크 수정 API")
     @PatchMapping("/{bookmarkId}")
     fun updateBookmark(
-        @PathVariable bookmarkId: String,
-        @RequestBody @Valid bookmark: Bookmark.UpdateBookmarkDto
+        @PathVariable @ApiParam(value = "북마크 ID", example = "10", required = true) bookmarkId: String,
+        @RequestBody @Valid @ApiParam(value = "북마크 수정 정보", required = true) bookmark: Bookmark.UpdateBookmarkDto
     ): ResponseEntity<String> {
         bookmarkService.updateBookmark(bookmarkId, bookmark)
         return ResponseEntity.status(HttpStatus.OK).body("업데이트됨")
     }
 
+    @ApiOperation(value = "북마크 이동 API")
     @PatchMapping("/move/{bookmarkId}")
     fun moveBookmark(
-        @PathVariable bookmarkId: String,
-        @RequestBody bookmark: Bookmark.MoveBookmarkDto
+        @PathVariable @ApiParam(value = "북마크 ID", example = "10", required = true) bookmarkId: String,
+        @RequestBody @ApiParam(value = "북마크 이동 정보", required = true) bookmark: Bookmark.MoveBookmarkDto
     ): ResponseEntity<String> {
         bookmarkService.moveBookmark(bookmarkId, bookmark)
         return ResponseEntity.status(HttpStatus.OK).body("폴더가 이동됨")
     }
 
+    // TODO: 2021/12/04 Swagger 추가
     @GetMapping("/search/{keyWord}")
     fun searchBookmarkList(
         request: HttpServletRequest,
