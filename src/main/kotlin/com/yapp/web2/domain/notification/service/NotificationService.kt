40017d1f3d21c3610d8357174fa905d079ba8b24
JuHyun419 <zzang9ha@naver.com>
2021-12-11 03:52:30 +0900

    [#32] feat(remind)  - remind alarm update(toggle) api  - remind alarm cycle update api  - request dto

--- src/main/kotlin/com/yapp/web2/domain/notification/service/NotificationService.kt
+++ src/main/kotlin/com/yapp/web2/domain/notification/service/NotificationService.kt
@@ -4,13 +4,18 @@ import com.yapp.web2.domain.account.entity.Account
 import com.yapp.web2.domain.account.repository.AccountRepository
 import com.yapp.web2.domain.bookmark.entity.Bookmark
 import com.yapp.web2.domain.bookmark.repository.BookmarkRepository
+import com.yapp.web2.domain.notification.entity.dto.RemindCycleRequest
+import com.yapp.web2.domain.notification.entity.dto.RemindToggleRequest
+import com.yapp.web2.security.jwt.JwtProvider
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
 
 @Service
 class NotificationService(
     private val bookmarkRepository: BookmarkRepository,
-    private val accountRepository: AccountRepository
+    private val accountRepository: AccountRepository,
+    private val jwtProvider: JwtProvider
 ) {
     fun getRemindBookmark(): List<Bookmark> {
         val now = LocalDate.now()
@@ -20,5 +25,26 @@ class NotificationService(
     // notificaiton 부분 추가
     fun sendNotification(userId: Long) {
         // userId에 해당하는 account를 들고와서 전송시켜야한다.
+    }
+
+    @Transactional
+    fun changeRemindAlarm(request: RemindToggleRequest, accessToken: String) {
+        val userId = jwtProvider.getIdFromToken(accessToken)
+
+        accountRepository.findById(userId).ifPresent {
+            it.remindToggle = request.remindToggle
+            accountRepository.save(it)
+        }
+    }
+
+    @Transactional
+    fun updateRemindAlarmCycle(request: RemindCycleRequest, accessToken: String) {
+        val userId = jwtProvider.getIdFromToken(accessToken)
+
+        accountRepository.findById(userId).ifPresent {
+            it.remindCycle = request.remindCycle
+            accountRepository.save(it)
+        }
+
     }
 }
