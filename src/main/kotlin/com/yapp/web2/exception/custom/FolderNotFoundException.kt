7c18e5923ae8b82f70a5a273ad1abb80be4eeb43
JuHyun419 <zzang9ha@naver.com>
2021-12-12 17:02:15 +0900

    [#25] refactor(Folder)  - Add CustomException Class

--- src/main/kotlin/com/yapp/web2/exception/custom/FolderNotFoundException.kt
+++ src/main/kotlin/com/yapp/web2/exception/custom/FolderNotFoundException.kt
@@ -1,6 +1,6 @@
 package com.yapp.web2.exception.custom
 
 import com.yapp.web2.exception.BusinessException
-import com.yapp.web2.util.Message
+import com.yapp.web2.util.ExceptionMessage
 
-class FolderNotFoundException : BusinessException(Message.FOLDER_NOT_FOUND)
+class FolderNotFoundException : BusinessException(ExceptionMessage.FOLDER_NOT_FOUND)
