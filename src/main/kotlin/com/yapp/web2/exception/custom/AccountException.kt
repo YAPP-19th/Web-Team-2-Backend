211ac2e21310991224bc0e6ab53adb8c6f7cd08d
Ji-Ha <xodhkd36@naver.com>
2021-12-18 21:35:10 +0900

    [#25] refactor(account): - accountFromId 로직 사용하여 코드 재사용화 추가 - exception 리펙토링

--- src/main/kotlin/com/yapp/web2/exception/custom/AccountException.kt
+++ src/main/kotlin/com/yapp/web2/exception/custom/AccountException.kt
@@ -4,3 +4,5 @@ import com.yapp.web2.exception.BusinessException
 import com.yapp.web2.util.Message
 
 class ExistNameException: BusinessException(Message.EXIST_NAME)
+
+class ImageNotFoundException: BusinessException(Message.IMAGE_NOT_FOUND)
