4d2854052bc44ffb33ba643e23902c00e8790334
JuHyun419 <zzang9ha@naver.com>
2021-12-20 21:58:40 +0900

    [#86] refactor : change class file name

--- src/main/kotlin/com/yapp/web2/exception/custom/ExistNameException.kt
+++ src/main/kotlin/com/yapp/web2/exception/custom/ExistNameException.kt
@@ -3,4 +3,4 @@ package com.yapp.web2.exception.custom
 import com.yapp.web2.exception.BusinessException
 import com.yapp.web2.util.Message
 
-class ExistNameException: BusinessException(Message.EXIST_NAME)
+class ExistNameException : BusinessException(Message.EXIST_NAME)
