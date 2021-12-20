986066e72193eab2020ba9f6444c1a26de486fab
JuHyun419 <zzang9ha@naver.com>
2021-12-20 22:02:00 +0900

    [#86] chore(add ktlint plugins)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.5.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+
+    // ktlint
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    id("org.jlleitschuh.gradle.ktlint-idea") version "10.0.0"
+
     kotlin("jvm") version "1.5.21"
     kotlin("plugin.spring") version "1.5.21"
     kotlin("plugin.jpa") version "1.5.21"
@@ -36,25 +41,25 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation ("org.springframework.boot:spring-boot-starter-batch")
-    implementation ("io.jsonwebtoken:jjwt:0.9.1")
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+    implementation("io.jsonwebtoken:jjwt:0.9.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.springfox:springfox-boot-starter:3.0.0")
 
-    runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
     runtimeOnly("org.postgresql:postgresql")
 
     implementation("com.google.firebase:firebase-admin:6.8.1")
     implementation("com.squareup.okhttp3:okhttp:4.2.2")
 
     implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.228"))
-    implementation ("com.amazonaws:aws-java-sdk-s3")
+    implementation("com.amazonaws:aws-java-sdk-s3")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
         exclude(group = "org.mockito")
     }
-    testImplementation ("org.springframework.batch:spring-batch-test")
+    testImplementation("org.springframework.batch:spring-batch-test")
     testImplementation("com.ninja-squad:springmockk:2.0.3")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
