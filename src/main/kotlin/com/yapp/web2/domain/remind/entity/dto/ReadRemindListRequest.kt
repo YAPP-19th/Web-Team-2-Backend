31b900981d1c71f4003003ac2577486a144b1802
JuHyun419 <zzang9ha@naver.com>
2021-12-24 17:40:35 +0900

    [#32] refactor(remove annotation)

--- src/main/kotlin/com/yapp/web2/domain/remind/entity/dto/ReadRemindListRequest.kt
+++ src/main/kotlin/com/yapp/web2/domain/remind/entity/dto/ReadRemindListRequest.kt
@@ -6,7 +6,6 @@ import io.swagger.annotations.ApiModelProperty
 @ApiModel(description = "리마인드 읽음처리 할 북마크 ID 리스트")
 class ReadRemindListRequest(
 
-    //	"bookmarkIdList" : ["61bdbbaa72b0f85372ad57c8", "61bdbbaa72b0f8551ac8adn", "72cdaa72b0f1fv34272ad57c8"]
     @ApiModelProperty(
         value = "북마크 ID 리스트",
         required= true,
