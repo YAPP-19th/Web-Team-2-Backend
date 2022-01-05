37031808ac1aa349fc1dab7c59834c1728d0bd58
JuHyun419 <zzang9ha@naver.com>
2022-01-05 21:33:31 +0900

    [#32] refactor(remove putData)

--- src/main/kotlin/com/yapp/web2/infra/fcm/FirebaseService.kt
+++ src/main/kotlin/com/yapp/web2/infra/fcm/FirebaseService.kt
@@ -12,23 +12,22 @@ class FirebaseService {
 
     private val log = LoggerFactory.getLogger(javaClass)
 
-    fun sendMessage(targetToken: String, key: String, value: String, title: String, body: String) {
+    fun sendMessage(targetToken: String, title: String, body: String) {
         // 로컬 테스트
 //        val firebaseInit = FirebaseInit()
 //        firebaseInit.init()
 
         val notification = makeNotification(title, body)
-        val message = makeMessage(targetToken, key, value, notification)
+        val message = makeMessage(targetToken, notification)
         val firebaseApp = FirebaseApp.getInstance()
         val response = FirebaseMessaging.getInstance(firebaseApp).send(message)
         log.info("Firebase Cloud Messaging Response : $response")
     }
 
-    fun makeMessage(targetToken: String, key: String, value: String, notification: Notification): Message {
+    fun makeMessage(targetToken: String, notification: Notification): Message {
         return Message.builder()
             .setToken(targetToken)
             .setNotification(notification)
-            .putData(key, value)
             .build()
     }
 
