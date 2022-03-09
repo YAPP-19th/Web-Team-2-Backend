d66d9f51882d678d9c87f6c947737ad05e333ed6
JuHyun419 <zzang9ha@naver.com>
2022-03-09 23:35:32 +0900

    [#108] chore(query)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -128,3 +128,33 @@ ALTER TABLE account_folder
 DROP CONSTRAINT fkq12fenyrw4agbqxdkoe9j9t0,
 	ADD CONSTRAINT fkq12fenyrw4agbqxdkoe9j9t0
 	FOREIGN KEY (folder_id) REFERENCES folder (id) ON DELETE CASCADE;
+
+-- 테이블 생성 쿼리 확인
+SELECT
+        'CREATE TABLE ' || relname || E'\n(\n' ||
+  array_to_string(
+    array_agg(
+      '    ' || column_name || ' ' ||  type || ' '|| not_null
+    )
+    , E',\n'
+  ) || E'\n);\n'
+from
+    (
+        SELECT
+            c.relname, a.attname AS column_name,
+            pg_catalog.format_type(a.atttypid, a.atttypmod) as type,
+            case
+                when a.attnotnull
+                    then 'NOT NULL'
+                else 'NULL'
+                END as not_null
+        FROM pg_class c,
+             pg_attribute a,
+             pg_type t
+        WHERE c.relname = 'folder'
+          AND a.attnum > 0
+          AND a.attrelid = c.oid
+          AND a.atttypid = t.oid
+        ORDER BY a.attnum
+    ) as tabledefinition
+group by relname;
