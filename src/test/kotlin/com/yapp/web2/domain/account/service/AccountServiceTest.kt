974a74a00137bc2735816b0ec68754e7569a0388
JuHyun419 <zzang9ha@naver.com>
2022-03-23 22:09:27 +0900

    [#25] refactor(test): 실패 테스트코드 일부 수정

--- src/test/kotlin/com/yapp/web2/domain/account/service/AccountServiceTest.kt
+++ src/test/kotlin/com/yapp/web2/domain/account/service/AccountServiceTest.kt
@@ -1,29 +1,31 @@
 package com.yapp.web2.domain.account.service
 
+import com.yapp.web2.config.S3Uploader
 import com.yapp.web2.domain.account.entity.Account
 import com.yapp.web2.domain.account.repository.AccountRepository
+import com.yapp.web2.domain.folder.service.FolderService
 import com.yapp.web2.exception.BusinessException
 import com.yapp.web2.security.jwt.JwtProvider
-import com.yapp.web2.config.S3Uploader
-import com.yapp.web2.domain.folder.service.FolderService
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.assertj.core.api.Assertions.assertThat
 import org.springframework.mock.web.MockMultipartFile
 import org.springframework.web.multipart.MultipartFile
 import java.util.*
-import kotlin.properties.Delegates
 
 internal class AccountServiceTest {
 
     @InjectMockKs
+    private lateinit var accountService: AccountService
+
+    @MockK
     private lateinit var folderService: FolderService
 
     @MockK
@@ -34,7 +36,6 @@ internal class AccountServiceTest {
 
     @MockK
     private lateinit var s3Uploader: S3Uploader
-    private lateinit var accountService: AccountService
 
     @BeforeEach
     internal fun init() {
@@ -81,6 +82,7 @@ internal class AccountServiceTest {
         fun `해당 닉네임을 다른 유저가 사용하고 있다면 예외를 던진다`() {
             //given
             every { accountRepository.findAccountByName(testNickName.nickName) } returns listOf(account)
+            every { jwtProvider.getAccountFromToken(testToken) } returns account
             val expectedException = BusinessException("이미 존재하는 닉네임입니다")
 
             //when
@@ -95,14 +97,15 @@ internal class AccountServiceTest {
         @Test
         fun `Account가 존재하지 않으면 예외를 던진다`() {
             //given
-            every { jwtProvider.getIdFromToken(testToken) } returns 1
+            every { jwtProvider.getAccountFromToken(testToken) } returns account
             every { accountRepository.findById(any()) } returns Optional.empty()
             val expectedException = BusinessException("계정이 존재하지 않습니다.")
 
             //when
-            val actualException = Assertions.assertThrows(BusinessException::class.java) {
+            val actualException = assertThrows(BusinessException::class.java) {
                 accountService.changeNickName(testToken, testNickName)
             }
+
             //then
             assertEquals(expectedException.message, actualException.message)
         }
@@ -110,7 +113,7 @@ internal class AccountServiceTest {
         @Test
         fun `닉네임이 변경된다`() {
             //given
-            every { jwtProvider.getIdFromToken(testToken) } returns 1
+            every { jwtProvider.getAccountFromToken(testToken) } returns account
             every { accountRepository.findById(any()) } returns Optional.of(account)
 
             //when
@@ -139,7 +142,7 @@ internal class AccountServiceTest {
         @Test
         fun `Account가 존재하지 않으면 예외를 던진다`() {
             //given
-            every { jwtProvider.getIdFromToken(testToken) } returns 1
+            every { jwtProvider.getAccountFromToken(testToken) } returns account
             every { accountRepository.findById(any()) } returns Optional.empty()
             every { s3Uploader.upload(any(), any()) } returns "good/test"
             val expectedException = BusinessException("계정이 존재하지 않습니다.")
@@ -155,9 +158,9 @@ internal class AccountServiceTest {
         @Test
         fun `이미 사진이 존재하지 않을 때(== 기본 이미지 일 때,) 예외를 던진다`() {
             //given
-            every { jwtProvider.getIdFromToken(testToken) } returns 1
+            every { jwtProvider.getAccountFromToken(testToken) } returns account
             every { accountRepository.findById(1) } returns Optional.of(account)
-            val expectedException = BusinessException("사진이 이미 존재하지 않습니다")
+            val expectedException = BusinessException("이미지가 존재하지 않습니다")
 
             //when
             val actualException = assertThrows(BusinessException::class.java) {
@@ -171,7 +174,7 @@ internal class AccountServiceTest {
         @Test
         fun `이미지가 삭제 되고, 기본 이미지로 변경된다`() {
             //given
-            every { jwtProvider.getIdFromToken(testToken) } returns 1
+            every { jwtProvider.getAccountFromToken(testToken) } returns account
             every { accountRepository.findById(1) } returns Optional.of(account)
             account.image = "testImageURL"
 
@@ -199,7 +202,7 @@ internal class AccountServiceTest {
         @Test
         fun `배경색이 변경된다`() {
             //given
-            every { jwtProvider.getIdFromToken(testToken) } returns 1
+            every { jwtProvider.getAccountFromToken(testToken) } returns account
             every { accountRepository.findById(1) } returns Optional.of(account)
 
             //when
