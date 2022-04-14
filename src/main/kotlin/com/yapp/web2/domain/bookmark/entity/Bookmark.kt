c1c2b2d7de4d7572b464c1be5a299744c237d9be
Ji-Ha <xodhkd36@naver.com>
2022-04-14 18:28:20 +0900

    [#10] fix(remind list): val로 진행하니, prod 서버에서 불러오지 못하는 오류 발생

--- src/main/kotlin/com/yapp/web2/domain/bookmark/entity/Bookmark.kt
+++ src/main/kotlin/com/yapp/web2/domain/bookmark/entity/Bookmark.kt
@@ -62,7 +62,7 @@ class Bookmark(
     var remindCheck: Boolean = false
     var remindStatus: Boolean = false
 
-    val remindList = mutableListOf<Remind>()
+    var remindList = mutableListOf<Remind>()
 
     @ApiModel(description = "북마크 수정 DTO")
     class UpdateBookmarkDto(
