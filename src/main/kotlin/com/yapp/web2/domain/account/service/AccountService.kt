3252a9fd91ef52f73aae6e32761bb59bb2af4e7e
JuHyun419 <zzang9ha@naver.com>
2022-05-05 21:10:20 +0900

    [#130] feat(회원 2차기능 구현): Controller  - Swagger 어노테이션 추가  - 일부 리팩토링  - 회원가입, 로그인, 비밀번호 조회&체크, 비밀번호 변경, 회원탈퇴 API 추가

--- src/main/kotlin/com/yapp/web2/domain/account/service/AccountService.kt
+++ src/main/kotlin/com/yapp/web2/domain/account/service/AccountService.kt
@@ -5,22 +5,28 @@ import com.yapp.web2.domain.account.repository.AccountRepository
 import com.yapp.web2.security.jwt.JwtProvider
 import com.yapp.web2.security.jwt.TokenDto
 import com.yapp.web2.config.S3Uploader
+import com.yapp.web2.domain.account.entity.AccountRequestDto
 import com.yapp.web2.domain.folder.service.FolderService
 import com.yapp.web2.exception.BusinessException
 import com.yapp.web2.exception.custom.ExistNameException
 import com.yapp.web2.exception.custom.ImageNotFoundException
+import com.yapp.web2.exception.custom.PasswordMismatchException
 import com.yapp.web2.util.Message
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
+import java.lang.IllegalStateException
 import javax.transaction.Transactional
 
 @Service
+@Transactional
 class AccountService(
     private val folderService: FolderService,
     private val accountRepository: AccountRepository,
     private val jwtProvider: JwtProvider,
-    private val s3Uploader: S3Uploader
+    private val s3Uploader: S3Uploader,
+    private val passwordEncoder: PasswordEncoder
 ) {
 
     @Value("\${extension.version}")
@@ -41,27 +47,51 @@ class AccountService(
     }
 
     fun oauth2LoginUser(dto: Account.AccountProfile): Account.AccountLoginSuccess {
-        var account = Account.profileToAccount(dto)
-        var isRegistered = true
-
-        account = when (val savedAccount = accountRepository.findByEmail(account.email)) {
-            null -> {
-                isRegistered = false
-                val account2 = createUser(account)
-                folderService.createDefaultFolder(account)
-                account2
-            }
-            else -> {
-                savedAccount.fcmToken = account.fcmToken
-                createUser(savedAccount)
-            }
-        }
-        val tokenDto = jwtProvider.createToken(account)
-
-        return Account.AccountLoginSuccess(tokenDto, account, isRegistered)
+        val account = Account.profileToAccount(dto)
+        val existAccount = accountRepository.findByEmail(account.email)
+        return signUpOrLogin(account, existAccount)
     }
 
-    @Transactional
+    private fun signUpOrLogin(
+        account: Account,
+        existAccount: Account?
+    ): Account.AccountLoginSuccess {
+        var isRegistered = true
+        var account2 = account
+
+        account2 = when (existAccount) {
+            null -> {
+                isRegistered = false
+                val newAccount = createUser(account)
+                folderService.createDefaultFolder(account)
+                newAccount
+            }
+            else -> {
+                existAccount.fcmToken = account2.fcmToken
+                createUser(existAccount)
+            }
+        }
+        return Account.AccountLoginSuccess(jwtProvider.createToken(account2), account2, isRegistered)
+    }
+
+    fun singUp(dto: AccountRequestDto.SignUpRequest): Account.AccountLoginSuccess {
+        if (accountRepository.findByEmail(dto.email) != null) {
+            throw IllegalStateException(Message.EXIST_USER)
+        }
+
+        val encryptPassword = passwordEncoder.encode(dto.password)
+        val nickName = getNickName(dto.email)
+        val newAccount = createUser(Account.signUpToAccount(dto, encryptPassword, nickName))
+        folderService.createDefaultFolder(newAccount)
+
+        return Account.AccountLoginSuccess(jwtProvider.createToken(newAccount), newAccount, false)
+    }
+
+    internal fun getNickName(email: String): String {
+        val atIndex = email.indexOf("@")
+        return email.substring(0, atIndex)
+    }
+
     fun createUser(account: Account): Account {
         return accountRepository.save(account)
     }
@@ -80,7 +110,6 @@ class AccountService(
         }
     }
 
-    @Transactional
     fun changeNickName(token: String, nextNickName: Account.NextNickName) {
         val account = jwtProvider.getAccountFromToken(token)
 
@@ -90,7 +119,6 @@ class AccountService(
         }
     }
 
-    @Transactional
     fun changeProfile(token: String, profileChanged: Account.ProfileChanged) {
         val account = jwtProvider.getAccountFromToken(token)
         account.let {
@@ -100,7 +128,6 @@ class AccountService(
         }
     }
 
-    @Transactional
     fun changeProfileImage(token: String, profile: MultipartFile): String {
         val account = jwtProvider.getAccountFromToken(token).let {
             it.image = s3Uploader.upload(profile, DIR_NAME)
@@ -109,7 +136,6 @@ class AccountService(
         return account.image
     }
 
-    @Transactional
     fun deleteProfileImage(token: String) {
         val account = jwtProvider.getAccountFromToken(token)
         account.let {
@@ -118,7 +144,6 @@ class AccountService(
         }
     }
 
-    @Transactional
     fun changeBackgroundColor(token: String, changeUrl: String) {
         val account = jwtProvider.getAccountFromToken(token)
         account.backgroundColor = changeUrl
@@ -128,4 +153,38 @@ class AccountService(
         return if (userVersion == extensionVersion) Message.LATEST_EXTENSION_VERSION
         else Message.OLD_EXTENSION_VERSION
     }
+
+    fun signIn(request: AccountRequestDto.SignInRequest): Account.AccountLoginSuccess? {
+        val account = accountRepository.findByEmail(request.email) ?: throw IllegalStateException(Message.NOT_EXIST_EMAIL)
+
+        if (!passwordEncoder.matches(request.password, account.password)) {
+            throw IllegalStateException(Message.USER_PASSWORD_MISMATCH)
+        }
+
+        return Account.AccountLoginSuccess(jwtProvider.createToken(account), account, false)
+    }
+
+    fun comparePassword(token: String, dto: AccountRequestDto.CurrentPassword): String {
+        val account = jwtProvider.getAccountFromToken(token)
+        if (!passwordEncoder.matches(dto.currentPassword, account.password)) {
+            throw PasswordMismatchException()
+        }
+        return Message.SAME_PASSWORD
+    }
+
+    fun changePassword(token: String, dto: AccountRequestDto.PasswordChangeRequest): String {
+        val account = jwtProvider.getAccountFromToken(token)
+        if (!passwordEncoder.matches(dto.currentPassword, account.password)) {
+            throw PasswordMismatchException()
+        }
+        account.password = passwordEncoder.encode(dto.newPassword)
+        return Message.CHANGE_PASSWORD_SUCCEED
+    }
+
+    fun softDelete(token: String) {
+        val account = jwtProvider.getAccountFromToken(token)
+        account.softDeleteAccount()
+    }
+
+
 }
