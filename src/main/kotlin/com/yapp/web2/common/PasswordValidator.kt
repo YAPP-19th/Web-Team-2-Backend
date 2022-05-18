6e2f3f6acdab705bdc87ce2a6b8bf313f914b316
JuHyun419 <zzang9ha@naver.com>
2022-05-18 22:42:03 +0900

    [#130] refactor: 테스트 코드를 위한 context 변수 nullable로 수정

--- src/main/kotlin/com/yapp/web2/common/PasswordValidator.kt
+++ src/main/kotlin/com/yapp/web2/common/PasswordValidator.kt
@@ -6,16 +6,16 @@ import javax.validation.ConstraintValidatorContext
 
 class PasswordValidator : ConstraintValidator<CustomPassword, String> {
 
-    override fun isValid(password: String, context: ConstraintValidatorContext): Boolean {
+    override fun isValid(password: String, context: ConstraintValidatorContext?): Boolean {
         // 특수문자 + (영문자 or 숫자) 조합으로 8~16자 사이
         val passwordRegex = "^(?=.*[0-9a-zA-Z])(?=.*[!@#\$%^&*])(?=\\S+\$).{8,16}\$"
         val regex = Regex(passwordRegex)
         val isValidPassword = password.matches(regex)
 
         if (!isValidPassword) {
-            context.disableDefaultConstraintViolation()
-            context.buildConstraintViolationWithTemplate(Message.PASSWORD_VALID_MESSAGE)
-                .addConstraintViolation()
+            context?.disableDefaultConstraintViolation()
+            context?.buildConstraintViolationWithTemplate(Message.PASSWORD_VALID_MESSAGE)
+                ?.addConstraintViolation()
         }
         return isValidPassword
     }
