dbf11fef114a840bf3f31dc319749492bffb3220
JuHyun419 <zzang9ha@naver.com>
2022-07-10 17:55:55 +0900

    [#140] feat & test: 랜덤 문자열 Util Class 생성(임시 비밀번호 로직에서 사용) 및 테스트

---
+++ src/test/kotlin/com/yapp/web2/util/RandomUtilsTest.kt
@@ -0,0 +1,47 @@
+package com.yapp.web2.util
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+internal class RandomUtilsTest {
+
+    @RepeatedTest(5)
+    fun `0 ~ 9 사이의 랜덤 숫자를 생성한다`() {
+        val randomNumber = RandomUtils.getRandomNumber(0, 9)
+
+        assertTrue(randomNumber in 0..9)
+    }
+
+    @RepeatedTest(5)
+    fun `12자리의 랜덤 문자열을 생성한다`() {
+        val randomString = RandomUtils.getRandomAlphanumeric(12)
+
+        assertThat(12).isEqualTo(randomString.length)
+    }
+
+    @Test
+    fun `임의의 특수문자를 생성한다`() {
+        val characters = "!@#$%^&*()"
+        val randomSpecialCharacter = RandomUtils.getRandomSpecialCharacter()
+
+        assertTrue(characters.contains(randomSpecialCharacter))
+    }
+
+    @Test
+    fun `문자열을 랜덤으로 섞는다`() {
+        val words = "fjdkDzcm3a!mckz"
+
+        val shuffledWords = RandomUtils.shuffleCharacters(words)
+
+        assertAll(
+            { assertThat(words.length).isEqualTo(shuffledWords.length) },
+            { assertThat(words.toSortedSet()).isEqualTo(shuffledWords.toSortedSet()) }
+        )
+    }
+
+}
+
+
