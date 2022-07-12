b981ecbc8cbfbc0bb019efe261c38383865f3399
Ji-Ha <xodhkd36@gmail.com>
2022-07-12 21:22:54 +0900

    [#132] refactor(bookmark) - 모든 도토리를 조회할 때 뜨는 오류는 AccountFolder의 Authority 타입 문제. -> 해결

--- src/main/kotlin/com/yapp/web2/domain/folder/entity/AccountFolder.kt
+++ src/main/kotlin/com/yapp/web2/domain/folder/entity/AccountFolder.kt
@@ -1,13 +1,7 @@
 package com.yapp.web2.domain.folder.entity
 
 import com.yapp.web2.domain.account.entity.Account
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.JoinColumn
-import javax.persistence.ManyToOne
-import javax.persistence.FetchType
-import javax.persistence.GenerationType
+import javax.persistence.*
 
 @Entity
 class AccountFolder(
@@ -29,5 +23,6 @@ class AccountFolder(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
 
+    @Enumerated(EnumType.STRING)
     var authority: Authority = Authority.NONE
 }
