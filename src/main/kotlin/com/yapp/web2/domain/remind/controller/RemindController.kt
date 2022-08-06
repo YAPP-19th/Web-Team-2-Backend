fa70d1b4de8822b27a02e06912335d9b9a2a0cc6
JuHyun419 <zzang9ha@naver.com>
2022-08-06 20:33:48 +0900

    [#140] refactor: import wildcard 제거

--- src/main/kotlin/com/yapp/web2/domain/remind/controller/RemindController.kt
+++ src/main/kotlin/com/yapp/web2/domain/remind/controller/RemindController.kt
@@ -11,7 +11,14 @@ import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletRequest
 
 @RestController
