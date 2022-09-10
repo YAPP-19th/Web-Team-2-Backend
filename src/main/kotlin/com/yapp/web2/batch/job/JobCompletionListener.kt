0cc9c389210ff0fee1956992e9ec644c26c37fc6
JuHyun419 <zzang9ha@naver.com>
2022-09-10 15:51:19 +0900

    [#140] refactor: SlackService DI로 변경, Job Success 시 슬랙 알림 추가

--- src/main/kotlin/com/yapp/web2/batch/job/JobCompletionListener.kt
+++ src/main/kotlin/com/yapp/web2/batch/job/JobCompletionListener.kt
@@ -1,6 +1,6 @@
 package com.yapp.web2.batch.job
 
-import com.yapp.web2.infra.slack.SlackServiceImpl
+import com.yapp.web2.infra.slack.SlackService
 import org.slf4j.LoggerFactory
 import org.springframework.batch.core.BatchStatus
 import org.springframework.batch.core.JobExecution
@@ -9,20 +9,27 @@ import org.springframework.stereotype.Component
 import java.util.stream.Collectors
 
 @Component
-class JobCompletionListener : JobExecutionListenerSupport() {
+class JobCompletionListener(
+    private val slackApi: SlackService
+) : JobExecutionListenerSupport() {
 
     private val log = LoggerFactory.getLogger(javaClass)
-    private val slackApi: SlackServiceImpl = SlackServiceImpl()
 
     override fun afterJob(jobExecution: JobExecution) {
         val jobStatus = jobExecution.status
         log.info("Job {} - {}", jobStatus, jobExecution.jobInstance.jobName)
 
+        if (jobStatus == BatchStatus.COMPLETED) {
+            slackApi.sendSlackAlarm(
+                "*`${jobExecution.jobInstance.jobName}`* - executed"
+            )
+        }
+
         if (jobStatus != BatchStatus.COMPLETED) {
             val errorMessage = String.format(
                 "*`Batch Error`* - %s", getErrorMessage(jobExecution).replace("\"", "")
             )
-            slackApi.sendMessage(errorMessage)
+            slackApi.sendSlackAlarm(errorMessage)
         }
     }
 
